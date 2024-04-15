e9604335f984e4454df1c98cbfc68e26c0cd588b
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-04-15 18:30:46 +0100

    Selects all previous buttons when button selected

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-const container = document.querySelector(`#energy-list`);
-const buttons = document.querySelectorAll(`button`);
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.style.backgroundColor === `rgb(6, 111, 6)`) {
-      button.style.backgroundColor = `rgb(11, 179, 11)`;
-    } else {
-      button.style.backgroundColor = `rgb(6, 111, 6)`;
-    }
+let energyCount = 0;
+
+const energyContainer = document.querySelector(`.energy-points`);
+const energyButtons = energyContainer.querySelectorAll(`button`);
+
+const assignEnergyButtonColours = () => {
+  energyButtons.forEach((energyButton) => {
+    energyButton.style.backgroundColor =
+      energyButton.id <= energyCount ? `rgb(11, 179, 11)` : `rgb(6, 111, 6)`;
+  });
+};
+
+energyButtons.forEach((energyButton) => {
+  energyButton.addEventListener(`click`, (item) => {
+    const id = item.target.id;
+    energyCount = id;
+    assignEnergyButtonColours();
   });
 });
