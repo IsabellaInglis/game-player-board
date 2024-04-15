6a4caeda6a4b26e307a4eda78062788814c165dd
IsabellaInglis <156921267+IsabellaInglis@users.noreply.github.com>
2024-04-15 18:55:31 +0100

    Point selection functionality added for all sections

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
-let energyCount = 0;
+const addButtonFunctionality = ({ containerSelector, colors }) => {
+  let energyCount = 0;
+  const energyContainer = document.querySelector(containerSelector);
+  const energyButtons = energyContainer.querySelectorAll(`button`);
 
-const energyContainer = document.querySelector(`.energy-points`);
-const energyButtons = energyContainer.querySelectorAll(`button`);
+  const assignEnergyButtonColours = () => {
+    energyButtons.forEach((energyButton) => {
+      energyButton.style.backgroundColor =
+        Number(energyButton.id) <= energyCount
+          ? colors.selected
+          : colors.unselected;
+    });
+  };
 
-const assignEnergyButtonColours = () => {
   energyButtons.forEach((energyButton) => {
-    energyButton.style.backgroundColor =
-      energyButton.id <= energyCount ? `rgb(11, 179, 11)` : `rgb(6, 111, 6)`;
+    energyButton.addEventListener(`click`, (item) => {
+      const id = item.target.id;
+      energyCount = id;
+      assignEnergyButtonColours();
+    });
   });
 };
+// add energy button functionality
+addButtonFunctionality({
+  containerSelector: `.energy-points`,
+  colors: { selected: `rgb(11, 179, 11)`, unselected: `rgb(6, 111, 6)` },
+});
 
-energyButtons.forEach((energyButton) => {
-  energyButton.addEventListener(`click`, (item) => {
-    const id = item.target.id;
-    energyCount = id;
-    assignEnergyButtonColours();
-  });
+// add block button functionality
+addButtonFunctionality({
+  containerSelector: `.block-list`,
+  colors: {
+    selected: `rgb(36, 138, 247)`,
+    unselected: `rgb(56, 117, 183)`,
+  },
+});
+
+// add HP functionality
+addButtonFunctionality({
+  containerSelector: `.hp-list`,
+  colors: {
+    selected: `rgb(247, 41, 41)`,
+    unselected: `rgb(159, 30, 30)`,
+  },
 });
